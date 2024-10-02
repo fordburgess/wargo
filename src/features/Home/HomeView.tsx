efa09131f2e49cb7754bc909d158d8c07fb08351
Ford Burgess <ford.burgess@abintus.consulting>
2024-10-02 17:02:13 +0100

    some styling and layout - custom font not working

--- src/features/Home/HomeView.tsx
+++ src/features/Home/HomeView.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const HomeView = () => {
   return (
-    <div>
-      This is the home view
+    <div className='h-screen w-screen flex items-center justify-center'>      
+      <div className='flex flex-col items-center'>
+        <h1 className='font-military text-4xl'>WARGO</h1>
+        <div>
+          <input className='large-rounded-input' />
+        </div>
+      </div>
     </div>
   )
 }
